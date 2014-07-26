b49e8a80ee195fa27b35e587440dcaf2490a265d
Chris McMacken <chris.mcmacken@gmail.com>
2014-07-25 20:51:55 -0500

    Connecting up the UISegmentControl and difficulty outlet

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,15 @@
 class MenuController < UIViewController
   extend IB
 
-  ib_outlet :difficulty_chooser, UISegmentedControl
-  ib_outlet :high_score_label, UILabel
+  outlet :difficulty_chooser, UISegmentedControl
+  outlet :high_score_label, UILabel
+
+  def prepareForSegue(segue, sender: sender)
+    if segue.identifier == 'PlayGame'
+      game_controller = segue.destinationViewController
+      game_controller.easy_mode = self.difficulty_chooser.selectedSegmentIndex == 0
+    else
+      puts "Unknown segue identifier #{segue.identifier}"
+    end
+  end
 end
